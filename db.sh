f8151ae1524a11427fc56a91c77287201335f501
MasterTos <MasterTos@yahoo.com>
2019-02-04 16:15:09 +0700

    init

---
+++ db.sh
@@ -0,0 +1,6 @@
+docker run -d --name proxy \
+               --network=kong-net \
+               -p 5432:5432 \
+               -e "POSTGRES_USER=kong" \
+               -e "POSTGRES_DB=kong" \
+               postgres:9.6
