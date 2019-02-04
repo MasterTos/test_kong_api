d61cb8a7f6356905a9944af79e9773bf9552a6c6
MasterTos <MasterTos@yahoo.com>
2019-02-04 16:35:40 +0700

    change network

--- db.sh
+++ db.sh
@@ -1,5 +1,5 @@
-docker run -d --name proxy \
-               --network=kong-net \
+docker run -d --name kong-database \
+               --network=proxy \
                -p 5432:5432 \
                -e "POSTGRES_USER=kong" \
                -e "POSTGRES_DB=kong" \
